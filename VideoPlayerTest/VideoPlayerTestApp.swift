fe74f17d2f1cfb5810bf93e035e1cce1da2158fe
rajveermalviya <rajveer0malviya@gmail.com>
2024-04-13 12:26:16 +0530

    Initial Commit

---
+++ VideoPlayerTest/VideoPlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTestApp.swift
+//  VideoPlayerTest
+//
+//  Created by Rajesh Malviya on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
